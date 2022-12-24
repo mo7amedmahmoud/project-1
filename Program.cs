f5fae64c52b9037b252b071a06c13b926b4608bf
mohamedmahmoud <120738292+mo7amedmahmoud@users.noreply.github.com>
2022-12-24 19:34:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project__1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a;
+            Console.WriteLine("\n    Welcome!");
+            Console.WriteLine("\n    Enter The Start :");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n    Enter The end :");
+            int end = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n    List of prime numbers between "+start +" and "+end +" :");
+            for (int i =start; i <= end; i++)
+            {
+                for ( a = 2; a<=i ;a++) 
+                {
+                    if (i%a==0)break;     
+                }
+                if (i == a)
+                    Console.WriteLine(i);    
+            }
+        }   
+    }
+}
